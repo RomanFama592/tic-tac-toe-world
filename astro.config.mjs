67d158dea656937d95f3a056509731537ba3bfb8
Famá Román <famaroman@gmail.com>
2024-02-08 01:38:38 -0300

    feat: add adapter & analytics vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
     server: {
         port: 8081
     },
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
     vite: {
         // use "usePolling: true" in case of using dev container, otherwise set to "false" 
         server: {
